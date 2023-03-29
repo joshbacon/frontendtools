5d06f47dd746cc05694cb4aded87894a551d5876
joshbacon <jdbacon@mta.ca>
2023-03-29 14:36:24 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// have a button to switch form list cards to a gallery
+// - this changes it from the current design to netflix style
+
+/*
+    UI Elements
+one     two        three    ....
+
+    Animation
+one      two      three     .....
+
+    etc.
+*/
